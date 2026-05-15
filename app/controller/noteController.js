f013abac7d8a6efd00297789651bcbd64cd677bc
Peanutbutterchicken <markjosephvictoriano20@gmail.com>
2026-05-15 14:28:54 +0800

    hydration and SSR

--- app/controller/noteController.js
+++ app/controller/noteController.js
@@ -2,8 +2,26 @@ let state = {
     notes: window.INITIAL_STATE || []
 };
 
-const containerNotes = document.querySelector('.container__notes');
-containerNotes.addEventListener('click', function(event){
-    console.log('clicked');
+function renderNote(note){
+    noteHtml =
+    `<div class="note">
+        <p class="text">"${note.text}"
+        <div class="stats">
+            <p class="time-elapsed">2 hours ago</p>
+            <span class="material-symbols-outlined icon-heart-like">favorite</span>
+        </div>
+    </div>`;
+    return noteHtml;
+}
 
-});
+function render(){
+    const containerNotes = document.querySelector('.container__notes');
+    containerNotes.innerHTML = '';
+
+    state.notes.forEach(note => {
+        const noteHtml = renderNote(note);
+        containerNotes.insertAdjacentHTML('beforeend', noteHtml);
+    });
+}
+
+render();
