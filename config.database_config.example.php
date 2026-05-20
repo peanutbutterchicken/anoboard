392c7b84dee158afd7aaeb109ee3415568d059c9
Peanutbutterchicken <markjosephvictoriano20@gmail.com>
2026-05-20 15:31:42 +0800

    chore: added database configuration example
    created an example template for new contributors to set up their local environments

---
+++ config.database_config.example.php
@@ -0,0 +1,9 @@
+<?php
+# modify to add your own xampp db creds
+# create a new 'config/' directory on root then move and rename this file into 'database_config.php'
+return [
+    'host' => 'localhost',
+    'dbname' => 'db_anoboard',
+    'username' => 'root',
+    'password' => ''
+];
