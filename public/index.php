af6a42b9976196d56c0865b21c586e3b9d605e96
mj <markjosephvictoriano20@gmail.com>
2026-05-21 22:45:07 +0800

    chore: Add root index.php to redirect traffic to public director

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once __DIR__ . '/../app/core/bootstrap.php';
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
